7595d552d847b92bb71555a1fd2c39f3aca8a41b
Brunohfc <bhenrick8@gmail.com>
2024-04-29 22:50:00 -0300

    project working until the knowledge experience

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+//interface
+import { IKnowledge } from '../../interface/IKnowledge.interface';
 
 @Component({
   selector: 'app-knowledge',
@@ -8,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
+    public knowledgeIcons = signal<IKnowledge[]>([
+      {
+        src: '/assets/icons/knowledge/angular.svg',
+        alt: 'angular icone conhecimento',
+      },
+      {
+        src: "assets/icons/knowledge/css3-alt.svg",
+        alt: "css icone conhecimento",
+      },
+      {
+        src: "/assets/icons/knowledge/css3-alt.svg",
+        alt: "html icone conhecimento",
+        
+      },
+      {
+        src: "/assets/icons/knowledge/js.svg",
+        alt: "js icone conhecimento",
+      }
 
+    
+    ])
 }
